ddc728d455edb1c8bcb1c0ffe168c7bb5bfb64ea
brendongama <brendon_gama@hotmail.com>
2022-07-22 13:29:19 -0300

    Controle de acesso por Roles

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.api.parkingcontrol.configs.security;
 
+import javax.transaction.Transactional;
+
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +12,7 @@ import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     final UserRepository userRepository;
@@ -21,6 +25,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-        return userModel;
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true,true, userModel.getAuthorities());
     }
 }
--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,12 +21,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		 http
-         	.httpBasic()
-         	.and()
-         	.authorizeHttpRequests()
-         	.anyRequest().authenticated()
-         	.and()
-         	.csrf().disable();
+			 .httpBasic()
+		     .and()
+		     .authorizeHttpRequests()
+		     .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+		     .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+		     .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+		     .anyRequest().authenticated()
+		     .and()
+		     .csrf().disable();
 	}
 	
 	@Override
